98481f90818ca1f4f40263b6661de9439951cbd5
En-regalia <mike.ranner1@gmail.com>
2024-04-26 10:52:01 +0100

    Updated search and location buttons to match style. Also added basic weather cards as well as inital user input manipulation

---
+++ script.js
@@ -0,0 +1,5 @@
+function updateForcast() {
+    var userInput = document.getElementById("userinput").value;
+    var forcastHeading = document.getElementById("forcastHeading");
+    forcastHeading.textContent = "Your 5 day forcast for" + userInput
+}
