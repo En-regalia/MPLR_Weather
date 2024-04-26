bd2d2d5d80d7771021f9317fe5210a02245b70e8
En-regalia <mike.ranner1@gmail.com>
2024-04-26 20:00:23 +0100

    added inital constants based on user input

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function updateForcast() {
+const searchButton = document.querySelector('.search-btn');
+const cityInput = document.querySelector('.city-input')
+const locationBUtton = document.querySelector('.location-btn')
+
+const api_key = "TBC" //Enter your API key for OpenWeatherMap
+
+function updateForcast() { //fucntion to update wether forcast heading
     var userInput = document.getElementById("userinput").value;
     var forcastHeading = document.getElementById("forcastHeading");
     forcastHeading.textContent = "Your 5 day forcast for" + userInput
 }
+
+
+
+searchButton.addEventListener("click", getCityCoordinates);
