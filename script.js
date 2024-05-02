9fd8204b53fd34649a097e328b3db6634f46454b
En-regalia <mike.ranner1@gmail.com>
2024-05-02 17:13:56 +0100

    Updated Local files before adding .gitignore file for API key managment

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ const locationBUtton = document.querySelector('.location-btn')
 
 const api_key = "TBC" //Enter your API key for OpenWeatherMap
 
-function updateForcast() { //fucntion to update wether forcast heading
-    var userInput = document.getElementById("userinput").value;
-    var forcastHeading = document.getElementById("forcastHeading");
-    forcastHeading.textContent = "Your 5 day forcast for" + userInput
-}
+//Need to update this function. Technical Debt. 
+//function updateForcast() { //fucntion to update wether forcast heading
+    //var userInput = document.getElementById("userinput").value;
+    //var forcastHeading = document.getElementById("forcastHeading");
+    //forcastHeading.textContent = "Your 5 day forcast for" + userInput
+//}
 
+const getCityCoordinates = () => {
+    const cityName = cityInput.value.trim();
+    if(!cityName) return;
+    API_url = 'http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_key}';
+    
+
+}
 
 
 searchButton.addEventListener("click", getCityCoordinates);
