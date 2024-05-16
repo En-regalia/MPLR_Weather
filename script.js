d8efaf647acc8f62383b11ebdf3070fdaf699dbc
En-regalia <mike.ranner1@gmail.com>
2024-05-16 16:36:57 +0100

    Updated JS code to fetch weather data from OpenWeather API. Added function in code to remove old weather cards and create new weather cards from the data pulled from the API call.

--- script.js
+++ script.js
@@ -3,42 +3,12 @@ const searchButton = document.querySelector('.search-btn');
 const cityInput = document.querySelector('.city-input');
 const locationBUtton = document.querySelector('.location-btn');
 const weatherCardsDiv = document.querySelector('.weather-cards');
+const currentCardsDiv = document.querySelector('.current-weather')
 
-const api_key = 'TEST_API' //Enter your API key for OpenWeatherMap in the .env file
+const api_key = "API_Key" //Enter your API key for OpenWeatherMap in the .env file
 
-const createWeatherCard = (weatherItem) => {
-    return `<li class="card">
-                <h3>(${weatherItem.dt_txt.split(' ')[0]})</h3>
-                <h6>Temp: ${weatherItem.main.temp}°C</h6>
-                <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
-                <h6>Humidity: ${weatherItem.main.humidity}% </h6>
-            </li>`;
-}
 
-const getWeatherdetails = (cityName, lat, lon) => {
-    const API_url = `http://api.openweathermap.org/data/2.5/forecast?units=metric&lat=${lat}&lon=${lon}&appid=${api_key}&cnt=40`;
-    
-    console.log(new Date(fiveDaysData.dt_txt).getDate())
-
-    fetch(API_url).then(res => res.json()).then(data => {
-        const ForcastDays = [];
-        const fiveDaysData = data.list.filter(forecast => {
-            const forcastDate = new Date(forecast.dt_txt).getDate();
-            if(!ForcastDays.includes(forcastDate)) {
-                return ForcastDays.push(forcastDate);
-            }
-        });
-        console.log(new Date(fiveDaysData.dt_txt).getDate());
-        console.log(fiveDaysData);
-        fiveDaysData.forEach(weatherItem => {
-            weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(weatherItem));
-        });
-    }).catch(() => {
-        alert('Error2: Error fetching locaiton issues')
-    })
-    }
-
-//Need to update this function. Technical Debt. 
+//Need to update this function.
 //function updateForcast() { //fucntion to update wether forcast heading
     //var userInput = document.getElementById("userinput").value;
     //var forcastHeading = document.getElementById("forcastHeading");
@@ -49,13 +19,73 @@ function getCityCoordinates() {
     const cityName = cityInput.value.trim();
     if (!cityName) return;
     const API_url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${api_key}`;
+    
     fetch(API_url).then(res => res.json()).then(data => {
-        const { name, lat, lon } = data[0];
-        getWeatherdetails(name, lat, lon);
-        // Option to change this to "return lat +'.'+ lon" or a derivative.
+
+        
+        const {name, lat, lon } = data[0]
+        getWeatherdetails(name, lat, lon)
+ 
     }).catch(() => {
         alert('Error1: Error fetching locaiton cordinates')
     })
+
+}
+
+const getWeatherdetails = (cityName, lat, lon) => {
+
+    const API_url = `http://api.openweathermap.org/data/2.5/forecast?units=metric&lat=${lat}&lon=${lon}&appid=${api_key}`;
+
+    fetch(API_url)
+        .then(res => res.json())
+        .then(data => {
+
+            const ForcastDaysList = [];
+            const fiveDaysForcast = data.list.filter(forecast => {
+                const forecastDate = new Date(forecast.dt_txt).getDate();
+                if (!ForcastDaysList.includes(forecastDate)) {
+                    return ForcastDaysList.push(forecastDate);
+                
+                }
+
+            });
+            
+            //resets cityInput value and remove exsisting weather cards
+            cityInput.value = "";
+            currentCardsDiv.innerHTML = "";
+            weatherCardsDiv.innerHTML = "";
+            
+            fiveDaysForcast.forEach((weatherItem, index) => {
+               if(index === 0) {
+                    currentCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index))
+               } else {
+                    weatherCardsDiv.insertAdjacentHTML("beforeend", createWeatherCard(cityName, weatherItem, index));
+               }
+            });
+        })
+        .catch(() => {
+            console.error('Error2');
+            alert('Error2: Error fetching location issues');
+        });
+};
+
+const createWeatherCard = (name, weatherItem, index) => {
+    if(index === 0 ) {
+        return `<div class="details">
+                    <h3> ${name} (${weatherItem.dt_txt.split(" ")[0]})</h3>
+                    <h6>Temperature: ${weatherItem.main.temp}°C</h6>
+                    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
+                    <h6>Humidity: ${weatherItem.main.humidity}% </h6>
+                </div>`;
+    } else {
+        return `<li class="card">
+                    <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+                    <h6>Temp: ${weatherItem.main.temp}°C</h6>
+                    <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
+                    <h6>Humidity: ${weatherItem.main.humidity}% </h6>
+                </li>`;
+    }
+    
 
 }
 
