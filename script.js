8932c2272030cc1ca4bb439e4c531ef34016b3be
En-regalia <mike.ranner1@gmail.com>
2024-05-17 11:57:49 +0100

    updated weather icon locaitions in current weather card to right if text

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const locationBUtton = document.querySelector('.location-btn');
 const weatherCardsDiv = document.querySelector('.weather-cards');
 const currentCardsDiv = document.querySelector('.current-weather')
 
-const api_key = 'Weather_API_Key' //Enter your API key for OpenWeatherMap in the .env file
+const api_key = '3aee46ab3ed5e73d34dde30783710867' //Enter your API key for OpenWeatherMap in the .env file
 
 
 function getCityCoordinates() {
@@ -32,15 +32,12 @@ const getWeatherdetails = (cityName, lat, lon) => {
     fetch(API_url)
         .then(res => res.json())
         .then(data => {
-
             const ForcastDaysList = [];
             const fiveDaysForcast = data.list.filter(forecast => {
                 const forecastDate = new Date(forecast.dt_txt).getDate();
                 if (!ForcastDaysList.includes(forecastDate)) {
                     return ForcastDaysList.push(forecastDate);
-                
                 }
-
             });
             
             //resets cityInput value and remove exsisting weather cards
@@ -58,20 +55,19 @@ const getWeatherdetails = (cityName, lat, lon) => {
         })
         .catch(() => {
             console.error('Error2');
-            alert('Error2: Error fetching location issues');
+            alert('Error2: Error fetching location data');
         });
 };
 
 const createWeatherCard = (name, weatherItem, index) => {
-    console.log(weatherItem)
     if(index === 0 ) {
         return `<div class="details">
                     <h3> ${name} (${weatherItem.dt_txt.split(" ")[0]})</h3>
-                    <h6><img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="Weather icon"></h6>
                     <h6>Temperature: ${weatherItem.main.temp}°C</h6>
                     <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
                     <h6>Humidity: ${weatherItem.main.humidity}% </h6>
-                </div>`;
+                </div>
+                <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="Weather icon">`;
     } else {
         return `<li class="card">
                     <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
@@ -84,6 +80,5 @@ const createWeatherCard = (name, weatherItem, index) => {
     
 
 }
-
 
 searchButton.addEventListener("click", getCityCoordinates);
